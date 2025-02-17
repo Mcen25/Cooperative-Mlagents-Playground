03ce12a825dfcd4e7150c52792c8d4793324a854
Matthew En <86386184+Mcen25@users.noreply.github.com>
2025-02-16 22:20:44 -0500

    Added soccer to scenes

--- CMaP/Assets/Scripts/AgentManager.cs
+++ CMaP/Assets/Scripts/AgentManager.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
 
-public class AgentManager : MonoBehaviour
+public class AgentManager : Agent
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject[] targetTransform;
+
+    public override void OnEpisodeBegin()
     {
-        
+        transform.localPosition = new Vector3(Random.Range(-4f, 4f), 0, Random.Range(-4f, 4f));
+        targetTransform[0].transform.localPosition = new Vector3(Random.Range(-4f, 4f), 0, Random.Range(-4f, 4f));
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void CollectObservations(VectorSensor sensor)
     {
-        
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform[0].transform.localPosition);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+
+        float moveSpeed = 1f;
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * moveSpeed * Time.deltaTime;
+        base.OnActionReceived(actions);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Horizontal");
+        continuousActionsOut[1] = Input.GetAxis("Vertical");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Target"))
+        {
+            AddReward(1f);
+            EndEpisode();
+        }
     }
 }
