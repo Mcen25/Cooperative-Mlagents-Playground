508ee4093f46cbe67d7ef809b629548a41db1d09
Matthew En <86386184+Mcen25@users.noreply.github.com>
2025-02-23 01:41:30 -0500

    Perception to Goal setup

--- CMaP/Assets/Scripts/AgentManager.cs
+++ CMaP/Assets/Scripts/AgentManager.cs
@@ -7,27 +7,52 @@ using Unity.MLAgents.Actuators;
 
 public class AgentManager : Agent
 {
-    [SerializeField] private GameObject[] targetTransform;
 
+    [Header("TEAM")]
+    public int teamID;
+
+    [SerializeField] private GameObject targetTransform;
+
+    [Header("Child Objects")]
+    [SerializeField] private GameObject raycastVisionObject;
+
+    [Header("HEALTH")] public int AgentHealth;
+
+    private Vector3 initialPosition;
+
+    public override void Initialize()
+    {
+        initialPosition = transform.localPosition;
+    }
     public override void OnEpisodeBegin()
     {
-        transform.localPosition = new Vector3(Random.Range(-4f, 4f), 0, Random.Range(-4f, 4f));
-        targetTransform[0].transform.localPosition = new Vector3(Random.Range(-4f, 4f), 0, Random.Range(-4f, 4f));
+        transform.localPosition = initialPosition;
+        targetTransform.transform.localPosition = new Vector3(Random.Range(-4f, 4f), 0, Random.Range(-4f, 4f));
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(transform.localPosition);
-        sensor.AddObservation(targetTransform[0].transform.localPosition);
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
         float moveX = actions.ContinuousActions[0];
         float moveZ = actions.ContinuousActions[1];
+        float sensorTilt = actions.ContinuousActions[2];
+        float bodyRotation = actions.ContinuousActions[3];
 
         float moveSpeed = 1f;
         transform.localPosition += new Vector3(moveX, 0, moveZ) * moveSpeed * Time.deltaTime;
+
+        float rotationSpeed = 100f;
+        transform.Rotate(0f, bodyRotation * rotationSpeed * Time.deltaTime, 0f);
+        
+        if (raycastVisionObject != null)
+        {
+            float tiltSpeed = 50f;
+            raycastVisionObject.transform.Rotate(sensorTilt * tiltSpeed * Time.deltaTime, 0f, 0f);
+        }
         base.OnActionReceived(actions);
     }
 
@@ -39,9 +64,15 @@ public class AgentManager : Agent
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Target"))
+        if (other.TryGetComponent<Goal>(out Goal goal))
         {
-            AddReward(1f);
+            AddReward(+1f);
+            EndEpisode();
+        }
+
+        if (other.TryGetComponent<Wall>(out Wall wall))
+        {
+            AddReward(-1f);
             EndEpisode();
         }
     }
